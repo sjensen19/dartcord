818437659a1b3e09d706498865563e6bad2a91da
Sven Jensen <sven.jensen@cbdevelopment.nl>
2023-01-10 02:04:08 +0100

    Added core library

---
+++ lib/dartcord_core.dart
@@ -0,0 +1,16 @@
+library dartcord;
+
+export 'src/client.dart';
+
+export 'src/structures/user.dart';
+export 'src/structures/text_channel.dart';
+export 'src/structures/guild.dart';
+export 'src/structures/message.dart';
+
+export 'src/structures/channel.dart';
+export 'src/structures/channel_type.dart';
+
+export 'src/structures/message_embed.dart';
+export 'src/structures/message_embed_author.dart';
+export 'src/structures/message_embed_footer.dart';
+export 'src/structures/message_embed_text_field.dart';
